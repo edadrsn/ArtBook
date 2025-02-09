5937944b77bcaafc2553fabb88b125926eeafb1b
Eda Dursun <edadrsn929@gmail.com>
2025-02-09 21:58:55 +0300

    launcher methods created

--- app/src/main/java/com/example/artbook/MainActivity2.java
+++ app/src/main/java/com/example/artbook/MainActivity2.java
@@ -36,12 +36,19 @@ import java.io.ByteArrayOutputStream;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private ActivityMain2Binding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;  //Galeriye gitmek için
+    ActivityResultLauncher<String> permissionLauncher;      //İzin istemek için
+    Bitmap selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        registerLauncher();
 
     }
 
@@ -60,9 +67,9 @@ public class MainActivity2 extends AppCompatActivity {
             //İzin isteme mantığı kullanıcıya açıklansın mı?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
                 //Snackbar ile kullanıcıya açıklama yap
-                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",new View.OnClickListener(){
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
                     @Override
-                    public void onClick(View view){
+                    public void onClick(View view) {
                         //İzin iste
 
                     }
@@ -74,8 +81,61 @@ public class MainActivity2 extends AppCompatActivity {
         } else {
             //İzin verildi galeriye git
             //Galeriye git görsel al geri gel
-            Intent intent=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         }
+
+    }
+
+    private void registerLauncher(){
+        //Galeriye gitmek için
+        activityResultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode()==RESULT_OK){
+                    Intent intentFromResult=result.getData();
+                    if(intentFromResult!=null){
+                        Uri imageData=intentFromResult.getData();
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                //Uri yı bitmapa çevirmek
+                                ImageDecoder.Source source = ImageDecoder.createSource(MainActivity2.this.getContentResolver(), imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }else{
+                                selectedImage=MediaStore.Images.Media.getBitmap(MainActivity2.this.getContentResolver(),imageData);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+                        }
+                        catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                else{
+
+                }
+
+            }
+        });
+
+        //İzin istemek için
+        permissionLauncher=registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    //İzin verildi
+                    Intent intentToGallery=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                }
+                else{
+                    //İzin verilmedi
+                    Toast.makeText(MainActivity2.this,"Permission needed!",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
 
 
     }
